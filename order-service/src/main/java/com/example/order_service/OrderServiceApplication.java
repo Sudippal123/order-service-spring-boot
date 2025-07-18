0a8e2981379f02cc96949f4a88eeddc8ad678551
Sudip Pal <sudippal1810@gmail.com>
2025-07-18 20:22:39 +0530

    added configuration to connect MySQL DB

--- order-service/src/main/java/com/example/order_service/OrderServiceApplication.java
+++ order-service/src/main/java/com/example/order_service/OrderServiceApplication.java
@@ -1,12 +1,17 @@
 package com.example.order_service;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		dotenv.entries().forEach((dotenvEntry )-> System.setProperty(dotenvEntry.getKey(),dotenvEntry.getValue()));
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
 
